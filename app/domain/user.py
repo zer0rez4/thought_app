5875d5c69f1b5843225700e9e90360d14742cab0
Danil <farafonov-d08@mail.ru>
2026-03-18 22:15:00 +0300

    add name for users

--- app/domain/user.py
+++ app/domain/user.py
@@ -1,6 +1,7 @@
 class User():
-    def __init__(self, user_id: int, user_email: str, hashed_password: str) -> None:
+    def __init__(self, user_id: int, user_email: str, hashed_password: str, user_name: str) -> None:
         self.user_id = user_id
         self.user_email = user_email
         self.hashed_password = hashed_password
+        self.user_name = user_name
         self.is_active = True
