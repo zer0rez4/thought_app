6a4e359267f15443a9a85c14623fdf5eaacf1def
Danil <farafonov-d08@mail.ru>
2026-04-03 14:45:15 +0300

    chanhed register (with db)

---
+++ app/database/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class UserBase(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+
+
+class ThoughtBase(Base):
+    __tablename__ = 'thoughts'
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey("users.id"),nullable=False)
+    is_public = Column(Boolean)
+
+
--- app/main.py
+++ app/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from api.auth import router as auth_router
 from api.thought import router as thought_router
 
+from database.models import Base
+from database.database import engine
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title='thoughts note')
 
 app.include_router(auth_router)
