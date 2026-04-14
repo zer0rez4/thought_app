2683958ec160912c01983f85ff2267760faab23b
Danil <farafonov-d08@mail.ru>
2026-04-14 10:49:41 +0300

    add show user, change user name and check if new_name empty

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api.auth import router as auth_router
 from api.thought import router as thought_router
+from api.user import router as user_router
 
 from database.models import Base
 from database.database import engine
@@ -11,6 +12,7 @@ app = FastAPI(title='thoughts note')
 
 app.include_router(auth_router)
 app.include_router(thought_router)
+app.include_router(user_router)
 
 @app.get('/')
 def test():
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
 
 
 class UserCreate(BaseModel):
@@ -16,3 +16,14 @@ class UserResponce(BaseModel):
     id: int
     email: str
     name: str
+
+
+class UserChangeName(BaseModel):
+    new_name: str
+    
+    @field_validator('new_name')
+    @classmethod
+    def user_name_not_empty(cls, name: str) -> str:
+        if not name.strip():
+            raise ValueError('The name can not be empty')
+        return name.strip()
