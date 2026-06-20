9f811ebb2b2ed2fe55162940dbbfeb84b700e00c
Danil <farafonovdan08@gmail.com>
2026-06-20 09:28:41 +0300

    password validation

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,11 +1,25 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr, field_validator, Field
 from schemas.thoughts import ThoughtResponse
 
 
 class UserCreate(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(..., min_length=6)
     name: str
+
+    @field_validator('password')
+    @classmethod
+    def check_password(cls, password: str) -> str:
+        if not password.strip():
+            raise ValueError('The password can not be empty')
+        return password
+
+    @field_validator('name')
+    @classmethod
+    def user_name_not_empty(cls, name: str) -> str:
+        if not name.strip():
+            raise ValueError('The name can not be empty')
+        return name.strip()
 
 
 class UserLogin(BaseModel):
@@ -32,6 +46,9 @@ class UserUpdate(BaseModel):
     @field_validator('new_name')
     @classmethod
     def user_name_not_empty(cls, name: str) -> str:
+        if name is None:
+            return name
+        
         if not name.strip():
             raise ValueError('The name can not be empty')
         return name.strip()
